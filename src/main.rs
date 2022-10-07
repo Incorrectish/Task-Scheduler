9492944f8c20254a2bcbb0ddf5a41db27c5506bb
Ishan Kar <ishan.kar.us@gmail.com>
2022-10-07 14:49:40 -0700

    Program appears to be completely working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use chrono;
-use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Write;
@@ -30,56 +29,65 @@ fn main() {
             "remove" => remove(args),
             "list" => list(),
             "help" => help(),
+            "clear" => clear(),
+            "background" => background(),
             _ => println!("No matching argument"),
         }
     } else {
-        loop {
-            // let contents = serde_json::to_string(&hmap).unwrap();
-            // puts the thread to sleep for 59 seconds, this is to ensure the program runs around every
-            // minute
-            let my_map: HashMap<[u32; 5], String> = deserialize();
-
-            println!("My map = {my_map:?}");
-
-            let time: chrono::DateTime<chrono::Local> = chrono::offset::Local::now();
-
-            let daily_time = time.time().to_string();
-            let date = time.date().to_string();
-
-            let time_day = convert_date(&date as &str, &daily_time as &str);
-            // let
-            let script_path = my_map.get(&time_day);
-            match script_path {
-                Some(path) => execute_script(path),
-                None => {},
-            }
-            thread::sleep(time::Duration::from_millis(30 * 1000));
-        }
+        help();
     }
+}
+
+fn background() {
+    loop {
+        // let contents = serde_json::to_string(&hmap).unwrap();
+        // puts the thread to sleep for 59 seconds, this is to ensure the program runs around every
+        // minute
+        let my_map: HashMap<[u32; 5], String> = deserialize();
+
+        let script_path = my_map.get(&current_time());
+        match script_path {
+            Some(path) => execute_script(path),
+            None => {}
+        }
+        thread::sleep(time::Duration::from_millis(59 * 1000));
+    }
+}
+
+fn current_time() -> [u32; 5] {
+    let time: chrono::DateTime<chrono::Local> = chrono::offset::Local::now();
+
+    let daily_time = time.time().to_string();
+    let date = time.date().to_string();
+
+    convert_date(&date as &str, &daily_time as &str)
 }
 
 fn execute_script(path: &str) {
     let trimmed_path = path.trim();
     let output = std::process::Command::new("bash")
-            .arg(trimmed_path)
-            .output()
-            .expect("Couldn't execute command");
+        .arg(trimmed_path)
+        .output()
+        .expect("Couldn't execute command");
     println!(
-        "stdout was \n {:?} \n stderr was \n {:?} ", String::from_utf8(output.stdout), String::from_utf8(output.stderr)
+        "stdout was \n {:?} \n stderr was \n {:?} ",
+        String::from_utf8(output.stdout).expect("Output unable to be parsed to UTF-8"),
+        String::from_utf8(output.stderr).expect("Output unable to be parsed to UTF-8")
     );
 }
 
 fn add(args: Vec<String>) {
     // deserialize the hasmap, add the entry to the hashmap, then reserialize it
     let mut my_map: HashMap<[u32; 5], String> = deserialize();
-    println!("Original map is {my_map:?}");
     my_map.insert(
         convert_date(&args[2] as &str, &args[3] as &str),
         args[4].clone(),
     );
-    println!("New map is {my_map:?}");
     serialize(my_map);
-    println!("content of the file is {}", fs::read_to_string(SERIALIZE).expect("Sadness"));
+    println!(
+        "content of the file is {}",
+        fs::read_to_string(SERIALIZE).expect("Sadness")
+    );
     // serialized_map = serde_json::to_string(&my_map).expect("Could not serialize");
     // let mut serialize_file = File::options()
     //     .write(true)
@@ -94,7 +102,6 @@ fn remove(args: Vec<String>) {
     // deserialize the hasmap, remove the entry from the hashmap, then reserialize it
     let mut my_map: HashMap<[u32; 5], String> = deserialize();
     my_map.remove(&convert_date(&args[2] as &str, &args[3] as &str));
-    println!("The updated map is {my_map:?}");
     serialize(my_map);
     // serialized_map = serde_json::to_string(&my_map).expect("Could not serialize");
     // let mut serialize_file = File::options()
@@ -126,7 +133,6 @@ fn convert_date(date: &str, time: &str) -> [u32; 5] {
 fn list() {
     // Read the hashmap from the file and then print it out, maybe want a custom print
     let my_map: HashMap<[u32; 5], String> = deserialize();
-    println!("Contents of my map is {my_map:?}");
     for (time, script) in my_map {
         println!("Will execute {} at {}", script.trim(), revert_date(time));
     }
@@ -196,17 +202,17 @@ fn serialize(map: HashMap<[u32; 5], String>) {
         .truncate(true)
         .open(SERIALIZE)
         .expect("File opening for writing serialized map failed");
-    to_write.write_all(b"");
+    let _ = to_write.write_all(b"");
     for (array, path_to_script) in map {
-        to_write.write_all(b"[");
+        let _ = to_write.write_all(b"[");
         for i in array {
-            to_write.write_all((&i.to_string() as &str).as_bytes());
-            to_write.write_all(b", ");
+            let _ = to_write.write_all((&i.to_string() as &str).as_bytes());
+            let _ = to_write.write_all(b", ");
         }
-        to_write.write_all(b"]");
-        to_write.write_all(b" = ");
-        to_write.write_all((&path_to_script as &str).as_bytes());
-        to_write.write_all(b"\n");
+        let _ = to_write.write_all(b"]");
+        let _ = to_write.write_all(b" = ");
+        let _ = to_write.write_all((&path_to_script as &str).as_bytes());
+        let _ = to_write.write_all(b"\n");
     }
 }
 
@@ -215,11 +221,10 @@ fn deserialize() -> HashMap<[u32; 5], String> {
     // format is
     // [num1, num2, num3, num4, num5] = String
     if let Ok(lines) = read_lines(SERIALIZE) {
-        let mut array = String::new();
         for line in lines {
-            println!("line is {line:?}");
             let mut path_to_script = String::new();
             if let Ok(line_result) = line {
+                let mut array = String::new();
                 let mut found = false;
                 for character in line_result.chars() {
                     if character == '=' && found == false {
@@ -234,11 +239,39 @@ fn deserialize() -> HashMap<[u32; 5], String> {
                 }
                 // key = "[u32, u32, u32, u32, u32]"
                 // value = "path/to/file"
+
+                let arr = get_array(&array[1..array.len() - 1]);
+                map.insert(arr, path_to_script.clone());
             }
-            map.insert(get_array(&array[1..array.len() - 1]), path_to_script);
         }
     }
     map
+}
+
+fn clear() {
+    let map = deserialize();
+    let mut new_map = HashMap::new();
+    for (time, script) in map {
+        if after(time, current_time()) {
+            new_map.insert(time, script);
+        }
+    }
+    serialize(new_map);
+}
+
+fn after(new_time: [u32; 5], current_time: [u32; 5]) -> bool {
+    if new_time[4] < current_time[4] {
+        return false;
+    } else if new_time[3] < current_time[3] {
+        return false;
+    } else if new_time[2] < current_time[2] {
+        return false;
+    } else if new_time[1] < current_time[1] {
+        return false;
+    } else if new_time[0] < current_time[0] {
+        return false;
+    } 
+    true
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
